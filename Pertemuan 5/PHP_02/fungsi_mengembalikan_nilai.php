9dc46450720cf263350fde8e4623e63ce47db41f
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-09-29 10:34:52 +0700

    Praktikum 5 - Fungsi

---
+++ Pertemuan 5/PHP_02/fungsi_mengembalikan_nilai.php
@@ -0,0 +1,9 @@
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Saya berusia ". hitungUmur (2005, 2024)." tahun<br/>";
+?>
+
