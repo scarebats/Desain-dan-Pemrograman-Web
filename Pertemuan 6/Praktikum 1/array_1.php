67752aea6d7ac3bdbf3d171c2d6ba39f37ff1581
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-10-05 14:30:35 +0700

    Praktikum 1 - Array

--- Pertemuan 6/Praktikum 1/array_1.php
+++ Pertemuan 6/Praktikum 1/array_1.php
@@ -7,9 +7,13 @@
     <?php
     $Listdosen = ["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha" ];
 
-    echo $Listdosen[2] . "<br>";
-    echo $Listdosen[0] . "<br>";
-    echo $Listdosen[1] . "<br>";
+    //echo $Listdosen[2] . "<br>";
+    //echo $Listdosen[0] . "<br>";
+    //echo $Listdosen[1] . "<br>";
+
+    foreach ($Listdosen as $dosen) {
+        echo $dosen . "<br>";
+    }
     ?>
 </body>
 </html>
