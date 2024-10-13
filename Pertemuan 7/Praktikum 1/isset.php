5b83332cb287a9e6b5d873c96aae4792f9f6fdf1
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-10-13 17:21:17 +0700

    Praktikum 1 - Isset

---
+++ Pertemuan 7/Praktikum 1/isset.php
@@ -0,0 +1,19 @@
+<?php
+$umur;
+if (isset($umur) && $umur >= 10) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditentukan.";
+}
+?>
+
+<br>
+
+<?php
+$data= array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
