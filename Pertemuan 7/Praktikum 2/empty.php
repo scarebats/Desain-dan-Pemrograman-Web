18ab389fa56cf8acde6b6081d9ac4e4b0f4c79a4
Ahmad Naufal Ilham <sadamdani28@gmail.com>
2024-10-13 17:28:12 +0700

    Praktikum 2 - Empty

---
+++ Pertemuan 7/Praktikum 2/empty.php
@@ -0,0 +1,18 @@
+<?php
+$muArray = array();
+if (empty($muArray)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
+?>
+
+<br>
+
+<?php
+if (empty($nonExistenVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+?>
